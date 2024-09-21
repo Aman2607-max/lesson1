6371730a4f1e928ebf7cc7209fe407d97112bfc3
Aman2607-max <sadatbekovamangeldi324@gmail.com>
2024-09-21 19:36:25 +0600

    Add files via upload

---
+++ uroki iz kursa.py
@@ -0,0 +1,9 @@
+print ('hi aman')
+print (5 + 5)
+print (9 // 9)
+print (type('5'))
+print (type(True) (type(False)))
+print (1,2,3,4, 5 * 9, 'hi aman')
+print (56 == 55 or 231 <= 148)
+print (32 >= 24 and 77 == 77)
+print (7465 <= 8674 and 2340 <= 5740 and 32567 == 5768 or 49328 >= 5827) #эти пройденные коды из первого же урока
